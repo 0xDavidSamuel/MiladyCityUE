3ea072dc90ff1ff0280fd7e6a5cfdf1b0cb90431
0xDavidSamuel <oxdavidsamuel@aol.com>
2026-01-19 19:32:19 -0800

    Add Experience.jsx with lighting and controls

---
+++ src/components/Experience.jsx
@@ -0,0 +1,20 @@
+import { OrbitControls, Environment } from '@react-three/drei'
+import AvatarCanvas from './AvatarCanvas'
+
+export default function Experience() {
+  return (
+    <>
+      <OrbitControls
+        target={[0, 1, 0]}
+        minDistance={2}
+        maxDistance={5}
+        minPolarAngle={Math.PI / 4}
+        maxPolarAngle={Math.PI / 2}
+      />
+      <Environment preset="studio" />
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[5, 5, 5]} intensity={1} />
+      <AvatarCanvas />
+    </>
+  )
+}
